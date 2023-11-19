fa15830672900c76d98b6baef78d9adeba9070cd
harshumaretiya <harsh.patel.hp846@gmail.com>
2023-11-19 18:14:51 +0530

    Annonate gem added

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,3 +1,36 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  actors      :string
+#  awards      :string
+#  box_office  :string
+#  country     :string
+#  director    :string
+#  dvd         :string
+#  genre       :string
+#  imdb_rating :string
+#  imdb_votes  :string
+#  language    :string
+#  metascore   :string
+#  plot        :text
+#  poster      :string
+#  production  :string
+#  rated       :string
+#  ratings     :text
+#  released    :string
+#  response    :boolean
+#  runtime     :string
+#  title       :string
+#  type        :string
+#  website     :string
+#  writer      :text
+#  year        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  imdb_id     :string
+#
 require "test_helper"
 
 class MovieTest < ActiveSupport::TestCase
